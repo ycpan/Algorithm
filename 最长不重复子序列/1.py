e782d90ea3cd3660056c360425fd8d762d4dbee2
ycpan <930101702@qq.com>
2021-03-30 13:15:25 +0800

    add comment to 最长不重复子序列

--- 最长不重复子序列/1.py
+++ 最长不重复子序列/1.py
@@ -1,11 +1,11 @@
 def solve(s):
-    # write your code here
     res = 0
     if s is None or len(s) == 0:
         return res
     d = {}
     tmp = 0
     start = 0
+    # 用start来记录字符串的初始位置，d记录字符串每个字符的位置，如果有重复，则进行更新。如果该字符串在后续中出现，则更新start。然后tmp记录当前字符串与start字符串的距离。tmp和res比较，选择最大的一个。
     for i in range(len(s)):
         if s[i] in d and d[s[i]] >= start:
             start = d[s[i]] + 1
