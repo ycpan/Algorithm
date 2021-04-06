ebbb2df71f0b1c64de60e031ce34834256135025
ycpan <930101702@qq.com>
2021-04-06 11:14:55 +0800

    add something

---
+++ longest_tree_layer/solve.py
@@ -0,0 +1,47 @@
+class Node():
+    def __init__(self,):
+        self.left = None
+        self.right = None
+    def 
+
+
+def get_longest_layer(node):
+    res_tmp = []
+    res = []
+    tmp = 0
+    count = 0
+    node_1 = []
+    node_2 = []
+    node_1.append(node)
+    while(node_1 or node_2):
+        res_tmp = []
+        for n in node_1:
+            if n:
+                res_tmp.append(n.value())
+                tmp += 1
+                if n.left != None:
+                    node_2.append(n.left)
+                if n.right !=None:
+                    node_2.append(n.right)
+        node_1 = []
+        if tmp >= count:
+            res = res_tmp
+            count = tmp
+        res_tmp = []
+        for n in node_2:
+            if n:
+                res_tmp.append(n.value())
+                tmp += 1
+                if n.left != None:
+                    node_1.append(n.left)
+                if n.right !=None:
+                    node_1.append(n.right)
+
+        node_2 = []
+        if tmp >= count:
+            res = res_tmp
+            count = tmp
+    return res,count
+
+
+
