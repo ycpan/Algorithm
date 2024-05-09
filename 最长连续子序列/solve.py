001658b6d46c0b6e792f2f96b12ab597763c6396
panyc <930101702@qq.com>
2024-05-09 09:47:54 +0800

    add 最长连续子序列长度

---
+++ 最长连续子序列/solve.py
@@ -0,0 +1,50 @@
+# leetcode 128
+#最长连续子序列
+#给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+#
+#请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+#
+# 
+#
+#示例 1：
+#
+#输入：nums = [100,4,200,1,3,2]
+#输出：4
+#解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+#示例 2：
+#
+#输入：nums = [0,3,7,2,5,8,4,6,0,1]
+#输出：9
+class Solution(object):
+    def longestConsecutive(self, nums):
+        hash_dict = dict()
+        
+        max_length = 0
+        for num in nums:
+            if num not in hash_dict:
+                # 当前num左右邻居是否存在,如果存在，获取左右邻居的连续最长长度
+                left = hash_dict.get(num - 1, 0)
+                right = hash_dict.get(num + 1, 0)
+                # 将左右邻居的最长长度相加，并加上当前长度，就是最新的长度 
+                cur_length = 1 + left + right
+                if cur_length > max_length:
+                    max_length = cur_length
+                
+                hash_dict[num] = cur_length
+                hash_dict[num - left] = cur_length
+                hash_dict[num + right] = cur_length
+                # hash_dict 存储当前num，以及当前num连续最左端和连续最右端的最长连续长度，如num=4时，有如下：
+                #hash_dict = {0: 1, 3: 2, 7: 2, 2: 4, 5: 4, 8: 2, 4: 4}
+                
+        return max_length
+if __name__ == '__main__':
+    nums = [0,3,7,2,5,8,4,6,0,1]
+    st = Solution()
+    s =st.longestConsecutive(nums)
+    print(s)
+
+
+#作者：江不知
+#链接：https://leetcode.cn/problems/longest-consecutive-sequence/solutions/3956/dong-tai-gui-hua-python-ti-jie-by-jalan/
+#来源：力扣（LeetCode）
+#著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
