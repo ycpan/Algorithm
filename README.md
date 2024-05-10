7a44c65d9bd4f10776771bfc715168e77f51c3ad
panyc <930101702@qq.com>
2024-05-10 11:01:36 +0800

    add and update

--- README.md
+++ README.md
@@ -14,4 +14,15 @@
 - [x] [反转链表(牛客)](./content/BM1.md)
 - [x] [二叉树最长宽度(前期积累)](./content/longest_tree_layer.md)
 - [x] [topK问题(前期积累)](./content/topK.md)
+- [x] [移动零(leetcode)](./content/283.md)
+- [x] [盛水最多的容器(leetcode)](./content/11.md)
+- [ ] [三数之和(leetcode)](./content/15.md)
+- [ ] [接雨水(leetcode)](./content/42.md)
+    - 和盛水最多容器区别在于这个柱子可以不为0，雨水量不能相乘,盛水最多容器的话可以相乘
 
+- [x] [无重复字符的最长子串(leetcode)](./content/3.md)
+    - 与[最长不含重复字符的子字符串(牛客)](./content/ONT22.md)一样
+- [ ] [找到子符串中所有字母的异位词(leetcode)](./content/438.md)
+    - 与[字母异位词分组(leetcode)](./content/49.md)的区别在于，一个是字符串，一个是数组，数组中是字符串
+    - 这个是两个字符
+- [ ] [和为k的子数组(leetcode)](./content/560.md)
--- content/test.py
+++ content/test.py
@@ -1,38 +1,27 @@
-def longest_common_subsequence_indexes(X, Y):
-    m = len(X)
-    n = len(Y)
-    # 创建一个二维数组来存储动态规划表
-    dp = [[0] * (n+1) for _ in range(m+1)]
-    # 填充动态规划表
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-    # 回溯以找到最长公共子序列
-    i = m
-    j = n
-    lcs = []
-    lcs_indexes = []
-    while i > 0 and j > 0:
-        if X[i-1] == Y[j-1]:
-            lcs.append(X[i-1])
-            lcs_indexes.append((i-1, j-1))  # 记录索引
-            i -= 1
-            j -= 1
-        elif dp[i-1][j] > dp[i][j-1]:
-            i -= 1
-        else:
-            j -= 1
-    # 由于我们是从后往前构建子序列的，所以需要将其反转
-    lcs = lcs[::-1]
-    lcs_indexes = lcs_indexes[::-1]
-    return lcs, lcs_indexes
-# 测试代码
-X = "ABCBDAB"
-Y = "BDCAB"
-lcs, lcs_indexes = longest_common_subsequence_indexes(X, Y)
-print("最长公共子序列:", lcs)
-print("对应索引:", lcs_indexes)
-
+from collections import defaultdict
+def subarray_sum(nums, k):
+    # 初始化哈希表和前缀和
+    count = defaultdict(int)
+    import ipdb
+    ipdb.set_trace()
+    count[0] = 1  # 初始化前缀和为0的出现次数为1
+    prefix_sum = 0
+    result = 0
+    # 遍历数组
+    for num in nums:
+        # 计算前缀和
+        prefix_sum += num
+        # 如果存在前缀和为 prefix_sum - k 的记录，则说明存在一个子数组的和为 k
+        if prefix_sum - k in count:
+            result += count[prefix_sum - k]
+        # 在哈希表中记录前缀和的出现次数
+        count[prefix_sum] += 1
+    return result
+# 示例数组
+nums1 = [1, 1, 1]
+k1 = 2
+nums2 = [1, 2, 3]
+k2 = 3
+# 计算和为 k 的子数组的个数
+subarray_sum(nums1, k1), subarray_sum(nums2, k2)
+(2, 2)
