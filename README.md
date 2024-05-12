ccbf96c07d13e30770de71b3fae427de8138a59e
panyc <930101702@qq.com>
2024-05-13 02:45:03 +0800

    add and update

--- README.md
+++ README.md
@@ -9,25 +9,30 @@
 - [x] [最长不含重复字符的子字符串(牛客)](./content/ONT22.md)
     - 要求原字符串连续
 - [x] [最长公共子序列(前期积累)](./content/lcs.md)
-- [ ] [所有公共子序列，并返回长度和索引(前期积累)](./content/所有公共子序列.md)
+- [x] [所有公共子序列，并返回长度和索引(前期积累)](./content/所有公共子序列.md)
 
 - [x] [反转链表(牛客)](./content/BM1.md)
 - [x] [二叉树最长宽度(前期积累)](./content/longest_tree_layer.md)
 - [x] [topK问题(前期积累)](./content/topK.md)
 - [x] [移动零(leetcode)](./content/283.md)
 - [x] [盛水最多的容器(leetcode)](./content/11.md)
-- [ ] [三数之和(leetcode)](./content/15.md)
-- [ ] [接雨水(leetcode)](./content/42.md)
+- [x] [三数之和(leetcode)](./content/15.md)
+- [x] [接雨水(leetcode)](./content/42.md)
     - 和盛水最多容器区别在于这个柱子可以不为0，雨水量不能相乘,盛水最多容器的话可以相乘
 
 - [x] [无重复字符的最长子串(leetcode)](./content/3.md)
     - 与[最长不含重复字符的子字符串(牛客)](./content/ONT22.md)一样
-- [ ] [找到子符串中所有字母的异位词(leetcode)](./content/438.md)
+- [x] [找到子符串中所有字母的异位词(leetcode)](./content/438.md)
     - 与[字母异位词分组(leetcode)](./content/49.md)的区别在于，一个是字符串，一个是数组，数组中是字符串
     - 这个是两个字符
-- [ ] [和为k的子数组(leetcode)](./content/560.md)
-- [ ] [滑动窗口最大值(leetcode)](./content/239.md)
-- [ ] [最小覆盖子串(leetcode)](./content/76.md)
+- [x] [和为k的子数组(leetcode)](./content/560.md)
+- [x] [滑动窗口最大值(leetcode)](./content/239.md)
+- [x] [最小覆盖子串(leetcode)](./content/76.md)     
+    和[找到子符串中所有字母的异位词(leetcode)](./content/438.md)进行比较:
+    - **相同点**:
+        - 都是从字符串中找子串
+    - **不同点**:
+        - 当前的是子串里面可以有其它字符，而另一个要求不能有其它字符；这就导致，虽然两者都用到词频，但是前这是在连续的字符串上建词频，而当前的是可以在不连续的字符串上建词频。所以前者比较的是词频是否相等，当前的比较的是有效的字符数量是否相等（只有当某个字符的频率大于等于目标字符的个数时，才算一次有效）。
 - [x] [最大子数组和(leetcode)](./content/53.md)
 - [x] [合并区间(leetcode)](./content/56.md)
 - [x] [转轮数组(leetcode)](./content/189.md)
--- content/test2.py
+++ content/test2.py
@@ -1,27 +1,56 @@
-def trap(height):
-    # 初始化左右指针和最大雨水量
-    left, right = 0, len(height) - 1
-    max_water = 0
-    left_max = right_max = 0
-    # 当左指针小于右指针时继续
-    while left < right:
-        # 更新左右柱子的最大高度
-        left_max = max(left_max, height[left])
-        right_max = max(right_max, height[right])
-        # 如果左边的柱子较矮
-        import ipdb
-        ipdb.set_trace()
-        if height[left] < height[right]:
-            # 计算当前雨水量，并加到最大雨水量中
-            max_water += left_max - height[left]
-            # 移动左指针
+def minWindow(s, t):
+    from collections import Counter
+    
+    # 初始化 need 和 window 计数器
+    need = Counter(t)
+    window = {}
+    
+    # 初始化左右指针
+    left = right = 0
+    
+    # 表示窗口中满足 need 条件的字符个数
+    valid = 0
+    
+    # 记录最小覆盖子串的起始位置和长度
+    start, length = 0, float('inf')
+    
+    while right < len(s):
+        # c 是将移入窗口的字符
+        c = s[right]
+        # 右移窗口
+        right += 1
+        
+        # 进行窗口内数据的一系列更新
+        if c in need:
+            window[c] = window.get(c, 0) + 1
+            if window[c] == need[c]:
+                valid += 1
+        
+        # 判断左侧窗口是否要收缩
+        while valid == len(need):
+            # 更新最小覆盖子串
+            if right - left < length:#这个条件判定是必要的，当达到这一步的时候，说明right已经固定下来了，右移left，不断缩小长度，只有在length大于right-left的时候，说明left移动的还没有超过理想值；当length小于等于left的时候，
+                start = left
+                length = right - left
+            else:
+                import ipdb
+                ipdb.set_trace()
+                print(left)
+            
+            # d 是将移出窗口的字符
+            d = s[left]
+            # 左移窗口
             left += 1
-        else:
-            # 计算当前雨水量，并加到最大雨水量中
-            max_water += right_max - height[right]
-            # 移动右指针
-            right -= 1
-    return max_water
-height1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-res = trap(height1)
-print(res)
+            
+            # 进行窗口内数据的一系列更新
+            if d in need:#如果d not in need，valid保持不变，while valid == len(need)就会一直有效，left就会一直右移。
+                if window[d] == need[d]:
+                    valid -= 1
+                window[d] -= 1 # windows[d] >= need[d]的，当windows[d] > need[d]的时候，只更新window[d]
+    
+    # 返回最小覆盖子串
+    return "" if length == float('inf') else s[start:start + length]
+# 示例
+s = "cabwefgewcwaefgcf"
+t = "cae"
+print(minWindow(s, t))  # 输出: "cwae"
